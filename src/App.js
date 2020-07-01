ec7d8577b58d09b37ca228c8bc29c1921bd9b4fe
torikul <torikul>
2020-07-01 22:18:47 +0600

    font load globally

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/navbar';
 import Home from './components/home';
 import Workspace from './components/workspace';
 import Explore from './components/explore';
+import Navbar2 from './components/navbar2';
 import './App.css';
 
 
@@ -19,12 +20,13 @@ class App extends Component {
   render() {
     return (
       <>
-        <Navbar />
+        <Navbar2 />
         <main>
           <Switch>
             <Route path="/" component={Home} exact />
             <Route path="/workspace" component={Workspace} exact />
             <Route path="/explore" component={Explore} exact />
+            {/* <Route path="/navbar2" component={Navbar2} exact /> */}
           </Switch>
         </main>
       </>
