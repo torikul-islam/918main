524948fc3e243112cda323e410087abc5d5be766
torikul <torikul>
2020-07-03 01:23:52 +0600

    footer component design

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,19 +3,29 @@ import './footer.css'
 class footer extends Component {
     render() {
         return (
-            <div className='footer'>
-                <div style={{ marginLeft: 40 }}>
-                    <div className='quick-Link'>Quick Links</div>
-                    <div> Links 1</div>
-                    <div>Links 1</div>
-                    <div>Links 1</div>
-                </div>
-                <div>
-                    <div> Contact Us</div>
-                    <div> About Us</div>
-                </div>
 
-                <div> Subscribe</div>
+            <div className='container-fluid' id='footer'>
+                <div className='container'>
+                    <div className='row footer-main'>
+                        <div className='col-lg-4 footer-left'>
+                            <h3>Quick Links</h3>
+                            <ul className='footer-link'>
+                                <li className='footer-item'>Moodboard Creator</li>
+                                <li className='footer-item'>Shop</li>
+                                <li className='footer-item'>Learn</li>
+                                <li className='footer-item'>Be Inspired</li>
+                            </ul>
+                        </div>
+
+                        <div className='col-lg-4'>
+                            <h2 className='f-contact'>Contact</h2>
+                        </div>
+                        <div className='col-lg-4'>
+                            <h2 className='about-us'>About Us</h2>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         );
     }
