4ba3eccb923ce5fd5d551cb0dcd5c42a8b9876bb
Moksedul <65646625+aplectrum@users.noreply.github.com>
2020-07-03 03:22:16 +0600

    update footer
    update footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,7 +7,7 @@ class footer extends Component {
             <div className='container-fluid' id='footer'>
                 <div className='container'>
                     <div className='row footer-main'>
-                        <div className='col-lg-4 footer-left'>
+                        <div className='col-sm-4 footer-left'>
                             <h3>Quick Links</h3>
                             <ul className='footer-link'>
                                 <li className='footer-item'>Moodboard Creator</li>
@@ -17,13 +17,13 @@ class footer extends Component {
                             </ul>
                         </div>
 
-                        <div className='col-lg-4'>
+                        <div className='col-sm-4'>
                             <ul className="contact-menu">
                                 <li>Contact</li>
                                 <li>About Us</li>
                             </ul>
                         </div>
-                        <div className='col-lg-4'>
+                        <div className='col-sm-4'>
                             <ul className="subscribe">
                                 <li><img src={require('../../Asset/Images/mail.png')} alt="mail.png" /></li>
                                 <li className="subslist">Subscribe</li>
