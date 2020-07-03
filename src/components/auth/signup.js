05e4c738afb48e741b76b9a8732cf54b67556479
Moksedul <65646625+aplectrum@users.noreply.github.com>
2020-07-03 21:19:30 +0600

    update signup

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './signup.css';
 
-
-
-
 class Signup extends Component {
     constructor(props) {
         super(props);
@@ -23,34 +20,37 @@ class Signup extends Component {
                     <div className='cross-icon'>
                         <img src={require('../../Asset/Icons/cross.png')} alt="" />
                     </div>
-                    <div className='row desc'>
-                        <div className='col-lg-6 signup-left'>
-                            <h2>Create a free account.</h2>
-                            <h5>Why sign up?</h5>
-                            <ul className=' '>
-                                <li className='signup-item'>Create your own moodboards</li>
-                                <li className='signup-item'>Receive personalized Recommendations</li>
-                                <li className='signup-item'>Organize your inspiration and shopping list in to spot</li>
-                            </ul>
-                            <div className='have-account'>
-                                <h6>Already have an account</h6>
-                                <h3><Link to="#">Login</Link></h3>
+                    <div className='row'>
+                        <div className='col-sm-6'>
+                            <div className='signup-left'>
+                                <h2>Create a free account.</h2>
+                                <h5>Why sign up?</h5>
+                                <ul className=' '>
+                                    <li className='signup-item'><img src={require('../../Asset/Icons/checked.png')} alt="checked.png" /><span className="modd_board">Create your own moodboards</span></li>
+                                    <li className='signup-item'><img src={require('../../Asset/Icons/checked.png')} alt="checked.png" /><span className="modd_board">Receive personalized Recommendations</span></li>
+                                    <li className='signup-item'><img src={require('../../Asset/Icons/checked.png')} alt="checked.png" /><span className="modd_board">Organize your inspiration and shopping list in to spot</span></li>
+                                </ul>
+                                <div className='have-account'>
+                                    <h6>Already have an account</h6>
+                                    <h3><Link to="#">Login</Link></h3>
+                                </div>
                             </div>
                         </div>
-                        <div className='col-lg-6 signup-right'>
+                        <div className='col-sm-6'>
+                            <div className='signup-right'>
+                                <div className='signup-form'>
 
-                            <div className='signup-form'>
+                                    <form onSubmit={this.handleSubmit}>
+                                        <div className="form-group">
+                                            <input type="email" className="form-control" id="email" placeholder="Email" autoComplete='off' />
+                                        </div>
+                                        <div className="form-group">
+                                            <input type="password" className="form-control" id="password" placeholder="Password" autoComplete='off' />
+                                        </div>
+                                        <button type="button" className="btn btn-signup">Sign Up</button>
+                                    </form>
 
-                                <form onSubmit={this.handleSubmit}>
-                                    <div className="form-group">
-                                        <input type="email" className="form-control" id="email" placeholder="Email" autoComplete='off' />
-                                    </div>
-                                    <div className="form-group">
-                                        <input type="password" className="form-control" id="password" placeholder="Password" autoComplete='off' />
-                                    </div>
-                                    <button type="button" className="btn btn-signup">Sign Up</button>
-                                </form>
-
+                                </div>
                             </div>
                         </div>
                     </div>
