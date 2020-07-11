d58d3e1ca554b05f65b909c8dddbb3deff81b347
Moksedul <65646625+aplectrum@users.noreply.github.com>
2020-07-11 14:16:35 +0600

    update few things

--- src/components/onboard/onboardQ3.js
+++ src/components/onboard/onboardQ3.js
@@ -23,21 +23,22 @@ class OnboardQ3 extends Component {
                             <h2>Are there specific pieces you're looking for?</h2>
                             <p>(Choose as many as you'd like.)</p>
                         </div>
-
-                        <div className='row my-4 width_fixed'>
-                            <div className='row'>
-                                {filterRoom && filterRoom.map((item, i) =>
-                                    <div className='col-sm-3' key={i}>
-                                        <div className='onboard-item'>
-                                            <img src={item.ref_img} alt="" />
-                                            <h6>{item.designed_by}</h6>
+                        <form>
+                            <div className='row my-4 width_fixed'>
+                                <div className='row'>
+                                    {filterRoom && filterRoom.map((item, i) =>
+                                        <div className='col-sm-3' key={i}>
+                                            <div className='onboard-item'>
+                                                <img src={item.ref_img} alt="" />
+                                                <h6>{item.designed_by}</h6>
+                                            </div>
                                         </div>
-                                    </div>
-                                )}
+                                    )}
+                                </div>
                             </div>
-                        </div>
-                        <GoBtn text='Next' onClick={() => openModal('boardName')} />
-                        <div className="text-center"><span className="Skip-btn-style"> Skip </span></div>
+                            <GoBtn text='Next' onClick={() => openModal('boardName')} />
+                            <div className="text-center"><span className="Skip-btn-style"> Skip </span></div>
+                        </form>
                     </div>
                 </div>
             </div>
