853cc79262f481b593a9fcac72f9ec6f45ac492e
Moksedul <65646625+aplectrum@users.noreply.github.com>
2020-07-12 15:48:54 +0600

    updated

--- src/components/common/slider/slider4.js
+++ src/components/common/slider/slider4.js
@@ -7,7 +7,7 @@ const Slider4 = ({ data }) => {
     return (
         <>
             {data && data.map(item =>
-                <div className="col-sm-3" key={item.uuid}>
+                <div className="col-md-3 col-sm-12" key={item.uuid}>
                     <div className="image-slide">
                         <img src={item.ref_img} alt="" />
                         <h6>{item.retailer || item.designed_by}</h6>
