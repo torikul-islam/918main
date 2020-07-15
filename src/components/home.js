1a8b68532c843f5f5c5672f8a69abc3aeb704382
torikul <torikul>
2020-07-15 18:04:29 +0600

    change home component class based to functional

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import HeaderHome from './headerHome';
 import Modal from './common/modal/modal';
 import Signup from './auth/signup';
@@ -10,45 +10,32 @@ import axios from 'axios';
 import ShopSlide from './shop/shopSlide';
 import InspiredSlider from './inspired/inspiredSlider';
 import Onboard from './onboard/onboard';
-import './home.css';
 import roomsTestData from '../../src/testData/rooms.json';
 import shopTestData from '../../src/testData/shop.json';
+import './home.css';
+import roomServices from '../services/roomServices';
 
 
-class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
 
-            modal: {
-                isOpen: false,
-                name: null
-            },
-            postDate: [],
-            rooms: [],
-            shop: [],
-            currentPage: 0,
-            pageSize: 4
-        }
-    }
 
-    openModal = (name) => {
-        const modal = { ...this.state.modal };
-        modal['isOpen'] = true;
-        modal['name'] = name;
-        this.setState({ modal });
-    };
-    closeModal = () => {
-        const modal = { ...this.state.modal };
-        modal['isOpen'] = false;
-        this.setState({ modal });
+function Home() {
+    const [modal, setModal] = useState({ isOpen: true, name: null });
+    const [post, setPost] = useState([]);
+    // const [rooms, setRooms] = useState([]);
+    const [shop, setShop] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [pageSize, setPageSize] = useState(4);
+
+
+    function openModal(name) {
+        setModal({ isOpen: true, name: name })
     };
 
-    componentDidMount() {
-        this.setState({ postDate: roomsTestData.results, shop: shopTestData.results });
-    }
+    function closeModal() {
+        setModal({ isOpen: false, name: null })
+    };
 
-    onPageChange = (val) => {
+    function onPageChange(val) {
         if (val === '-') {
             this.setState({ currentPage: this.state.currentPage - 1 })
         } else {
@@ -56,54 +43,111 @@ class Home extends Component {
         }
     }
 
-    getShopData = async () => {
-        const { data } = await axios.get('/products/');
-        this.setState({ shop: data })
-    };
-
-    getInspiredData = async () => {
-        const { data } = await axios.get('/inspirations/');
-        this.setState({ inspired: data })
-    };
-
-    render() {
-        const { isOpen, name } = this.state.modal;
-        const { postDate, rooms, shop, currentPage, pageSize } = this.state;
-        return (
-            <>
-                <HeaderHome data={postDate.slice(0, 1)} openModal={this.openModal} />
-                <SliderPost data={postDate.slice(1, 5)} />
-                <InspiredSlider />
-                <SliderPost data={postDate.slice(5, 9)} />
-                <ShopSlide data={shop} currentPage={currentPage} pageSize={pageSize} onPageChange={this.onPageChange} />
-                <SliderPost data={postDate && postDate.slice(9, 13)} />
+    useEffect(() => {
+        (async function () {
+            // const { data } = await productServices.getAllProducts();
+            setShop(shopTestData.results);
+            setPost(roomsTestData.results);
+        })()
+    }, []);
 
 
 
-                {/* blow are extra....*/}
+    const { isOpen, name } = modal;
+    return (
+        <>
+            <HeaderHome data={post.slice(0, 1)} openModal={openModal} />
+            <SliderPost data={post.slice(1, 5)} />
+            <InspiredSlider />
+            <SliderPost data={post.slice(5, 9)} />
+            <ShopSlide data={shop} currentPage={currentPage} pageSize={pageSize} onPageChange={onPageChange} />
+            <SliderPost data={post.slice(9, 13)} />
 
-                {/* <SliderPost data={postDate && postDate.results.slice(9, 13)} /> */}
-
-                {/* <PostSlide />
-                <LearnButton />
-                <Inspired />
-                <SeeMoreButton />
-                <PostSlide />
-                <LearnButton />
-                <Inspired />
-                <SeeMoreButton />
-                <PostSlide /> */}
-
-                {name === 'signup' && <Modal isOpen={isOpen} childComp={<Signup openModal={this.openModal} closeModal={this.closeModal} />} />}
-                {name === 'loginNext' && <Modal isOpen={isOpen} childComp={<LoginNext {...this.props} openModal={this.openModal} closeModal={this.closeModal} />} />}
-                {name === 'boardName' && <Modal isOpen={isOpen} childComp={<BoardName openModal={this.openModal} closeModal={this.closeModal} />} />}
-                {name === 'createBoard' && <Modal isOpen={isOpen} childComp={<CreateBoard openModal={this.openModal} closeModal={this.closeModal} />} />}
-                {name === 'onboard' && <Modal isOpen={isOpen} childComp={<Onboard openModal={this.openModal} closeModal={this.closeModal} />} />}
-                {/* {name === 'onboardQ2' && <Modal isOpen={isOpen} childComp={<OnboardQ2 openModal={this.openModal} closeModal={this.closeModal} />} />} */}
-                {/* {name === 'onboardQ3' && <Modal isOpen={isOpen} childComp={<OnboardQ3 openModal={this.openModal} closeModal={this.closeModal} />} />} */}
-            </>
-        );
-    }
+            {name === 'signup' && <Modal isOpen={isOpen} childComp={<Signup openModal={openModal} closeModal={closeModal} />} />}
+            {name === 'loginNext' && <Modal isOpen={isOpen} childComp={<LoginNext openModal={openModal} closeModal={closeModal} />} />}
+            {name === 'boardName' && <Modal isOpen={isOpen} childComp={<BoardName openModal={openModal} closeModal={closeModal} />} />}
+            {name === 'createBoard' && <Modal isOpen={isOpen} childComp={<CreateBoard openModal={openModal} closeModal={closeModal} />} />}
+            {name === 'onboard' && <Modal isOpen={isOpen} childComp={<Onboard openModal={openModal} closeModal={closeModal} />} />}
+        </>
+    );
 }
 
 export default Home;
+
+
+
+
+
+// class Home extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             modal: {
+//                 isOpen: false,
+//                 name: null
+//             },
+//             postDate: [],
+//             rooms: [],
+//             shop: [],
+//             currentPage: 0,
+//             pageSize: 4
+//         }
+//     }
+
+//     openModal = (name) => {
+//         const modal = { ...this.state.modal };
+//         modal['isOpen'] = true;
+//         modal['name'] = name;
+//         this.setState({ modal });
+//     };
+//     closeModal = () => {
+//         const modal = { ...this.state.modal };
+//         modal['isOpen'] = false;
+//         this.setState({ modal });
+//     };
+
+//     componentDidMount() {
+//         this.setState({ postDate: roomsTestData.results, shop: shopTestData.results });
+//     }
+
+//     onPageChange = (val) => {
+//         if (val === '-') {
+//             this.setState({ currentPage: this.state.currentPage - 1 })
+//         } else {
+//             this.setState({ currentPage: this.state.currentPage + 1 })
+//         }
+//     }
+
+//     getShopData = async () => {
+//         const { data } = await axios.get('/products/');
+//         this.setState({ shop: data })
+//     };
+
+//     getInspiredData = async () => {
+//         const { data } = await axios.get('/inspirations/');
+//         this.setState({ inspired: data })
+//     };
+
+//     render() {
+//         const { isOpen, name } = this.state.modal;
+//         const { postDate, rooms, shop, currentPage, pageSize } = this.state;
+//         return (
+//             <>
+//                 <HeaderHome data={postDate.slice(0, 1)} openModal={this.openModal} />
+//                 <SliderPost data={postDate.slice(1, 5)} />
+//                 <InspiredSlider />
+//                 <SliderPost data={postDate.slice(5, 9)} />
+//                 <ShopSlide data={shop} currentPage={currentPage} pageSize={pageSize} onPageChange={this.onPageChange} />
+//                 <SliderPost data={postDate && postDate.slice(9, 13)} />
+
+//                 {name === 'signup' && <Modal isOpen={isOpen} childComp={<Signup openModal={this.openModal} closeModal={this.closeModal} />} />}
+//                 {name === 'loginNext' && <Modal isOpen={isOpen} childComp={<LoginNext {...this.props} openModal={this.openModal} closeModal={this.closeModal} />} />}
+//                 {name === 'boardName' && <Modal isOpen={isOpen} childComp={<BoardName openModal={this.openModal} closeModal={this.closeModal} />} />}
+//                 {name === 'createBoard' && <Modal isOpen={isOpen} childComp={<CreateBoard openModal={this.openModal} closeModal={this.closeModal} />} />}
+//                 {name === 'onboard' && <Modal isOpen={isOpen} childComp={<Onboard openModal={this.openModal} closeModal={this.closeModal} />} />}
+//             </>
+//         );
+//     }
+// }
+
+// export default Home;
