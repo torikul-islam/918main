5c7e7eeef5f59d5e1e678b6f52eafed960067143
Moksedul <mmislam1994@gmail.com>
2020-07-21 19:08:19 +0600

    updated navbar

--- src/components/nav/navbarB.js
+++ src/components/nav/navbarB.js
@@ -14,49 +14,50 @@ function NavbarB({ openModal, openMenu, handleOpenMenu, clickCard }) {
                         </Link>
                     </li>
                 </div>
-            </div>
-            <nav className="navbar-expand-lg navbar-light">
-                <button onClick={handleOpenMenu} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div style={openMenu ? { display: 'block' } : { display: 'none' }} className={`collapse navbar-collapse bg-lights ${openMenu ? 'show' : ''}`} id="navbarNav">
-                    <ul className="home-menu">
-                        <div className="search-float">
-                            <li className="search">
-                                <img src={require('../../Asset/Images/search.png')} alt="search.png" />
-                            </li>
-                        </div>
-                        <div className="logo-width">
-                            <li className='logo'>
-                                <Link className="nav-link" to="/" >
-                                    <img src={require('../../Asset/Images/Logo.png')} alt="Logo.png" />
-                                </Link>
-                            </li>
-                        </div>
-                        <div className="menu-float">
-                            <li className="menu-item font-hel">
-                                <Link className="nav-link" to="/workspace" exact >Workspace</Link>
-                            </li>
-                            <li className="menu-item font-hel">
-                                <Link className="nav-link" to="/explore" exact >Explore</Link>
-                            </li>
-                            <li className="menu-item signup" onClick={() => openModal('signup')}>
-                                <div className="nav-link" >Sign Up</div>
-                            </li>
-                            <span className="menu-icon">
-                                <li className="menu-item man">
-                                    <Link className="nav-link" to="/account" exact>
-                                        <img src={require('../../Asset/Images/man.png')} alt="man.png" />
+
+                <nav className="navbar-expand-lg navbar-light">
+                    <button onClick={handleOpenMenu} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div style={openMenu ? { display: 'block' } : { display: 'none' }} className={`collapse navbar-collapse bg-lights ${openMenu ? 'show' : ''}`} id="navbarNav">
+                        <ul className="home-menu">
+                            <div className="search-float">
+                                <li className="search">
+                                    <img src={require('../../Asset/Images/search.png')} alt="search.png" />
+                                </li>
+                            </div>
+                            <div className="logo-width">
+                                <li className='logo'>
+                                    <Link className="nav-link" to="/" >
+                                        <img src={require('../../Asset/Images/Logo.png')} alt="Logo.png" />
                                     </Link>
                                 </li>
-                                <li className="menu-item shop" onClick={clickCard}>
-                                    <img src={require('../../Asset/Images/black_shoping.png')} alt="black_shoping.png" />
+                            </div>
+                            <div className="menu-float">
+                                <li className="menu-item font-hel">
+                                    <Link className="nav-link" to="/workspace" exact >Workspace</Link>
                                 </li>
-                            </span>
-                        </div>
-                    </ul>
-                </div>
-            </nav>
+                                <li className="menu-item font-hel">
+                                    <Link className="nav-link" to="/explore" exact >Explore</Link>
+                                </li>
+                                <li className="menu-item signup" onClick={() => openModal('signup')}>
+                                    <div className="nav-link" >Sign Up</div>
+                                </li>
+                                <span className="menu-icon">
+                                    <li className="menu-item man">
+                                        <Link className="nav-link" to="/account" exact>
+                                            <img src={require('../../Asset/Images/man.png')} alt="man.png" />
+                                        </Link>
+                                    </li>
+                                    <li className="menu-item shop" onClick={clickCard}>
+                                        <img src={require('../../Asset/Images/black_shoping.png')} alt="black_shoping.png" />
+                                    </li>
+                                </span>
+                            </div>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         </div>
     )
 }
