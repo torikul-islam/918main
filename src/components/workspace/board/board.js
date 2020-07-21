3c5df1d8ee8edb206c1119b01bf108520b8ce2fb
Moksedul <mmislam1994@gmail.com>
2020-07-22 00:49:37 +0600

    border add

--- src/components/workspace/board/board.js
+++ src/components/workspace/board/board.js
@@ -3,24 +3,25 @@ import './board.css';
 
 const Board = props => {
     return (
-        <div className="container">
-            <div className="boards-fiter">
-                <ul>
-                    <li><h4>Boards</h4></li>
-                    <li><button className="filter">My Living Room</button></li>
-                    <li className="float-right"><img src={require('../../../Asset/Images/upicon.png')} alt="upicon.png" /></li>
-                </ul>
+        <div className="dragDrop">
+            <div className="container">
+                <div className="boards-fiter">
+                    <ul>
+                        <li><h4>Boards</h4></li>
+                        <li><button className="filter">My Living Room</button></li>
+                        <li className="float-right"><img src={require('../../../Asset/Images/upicon.png')} alt="upicon.png" /></li>
+                    </ul>
+                </div>
+                <div className="middle-body"></div>
+                <div className="bottom-icons">
+                    <ul>
+                        <li><img src={require('../../../Asset/Images/copypast.png')} alt="copypast.png" /></li>
+                        <li><img src={require('../../../Asset/Images/delete.png')} alt="delete.png" /></li>
+                        <li><img src={require('../../../Asset/Images/uparrwo.png')} alt="uparrwo.png" /></li>
+                        <li><img src={require('../../../Asset/Images/downarrow.png')} alt="downarrow.png" /></li>
+                    </ul>
+                </div>
             </div>
-            <div className="middle-body"></div>
-            <div className="bottom-icons">
-                <ul>
-                    <li><img src={require('../../../Asset/Images/copypast.png')} alt="copypast.png" /></li>
-                    <li><img src={require('../../../Asset/Images/delete.png')} alt="delete.png" /></li>
-                    <li><img src={require('../../../Asset/Images/uparrwo.png')} alt="uparrwo.png" /></li>
-                    <li><img src={require('../../../Asset/Images/downarrow.png')} alt="downarrow.png" /></li>
-                </ul>
-            </div>
-
         </div>
     );
 };
