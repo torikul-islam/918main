bbd04cbba2c7b03a093d801052e7438625c54009
jessanKhan <39450312+jessanKhan@users.noreply.github.com>
2020-07-24 01:44:54 +0600

    sidemenu

--- src/components/admin/sidemenu/sidemenu.js
+++ src/components/admin/sidemenu/sidemenu.js
@@ -2,95 +2,40 @@ import React, { Component } from 'react';
 import '../../admin/adminstyle.css';
 import '../fwstyle/font-awesome.min.css';
 import { Link } from 'react-router-dom';
-		
+
 class Sidemenu extends Component {
 
-	
-    render() {
-        return (
-            <div>
-                	<nav id="sidebar">
-			<div className="sidebar-header">
-				<h3>Admin Dashboard</h3>
-				<strong>AD</strong>
-			</div>
 
-			<ul className="list-unstyled components">
-				<li className="active">
-					<a href="dashboard.html">
-						<i className="fa fa-th-large"></i>
+	render() {
+		return (
+			<div>
+				<nav id="sidebar">
+					<div className="sidebar-header">
+						<h3>Admin Dashboard</h3>
+						<strong>AD</strong>
+					</div>
+
+					<ul className="list-unstyled components">
+						<li className="active">
+							<a href="dashboard.html">
+								<i className="fa fa-th-large"></i>
 						Dashboard
 					</a>
-				</li>
-				<li>
-					<Link to ="/admin"> Products</Link>
-				</li>
-				<li>
-					<a href="#" data-toggle="collapse" aria-expanded="false">
-						<i className="fa fa-users"></i>
-						Rooms
-					</a>
-					<ul className="collapse list-unstyled" id="pageSubmenu1">
-						<li>Add Rooms</li>
-						<li>Rooms List</li>
+						</li>
+						<li>
+							<Link to="/add-product">Add Products</Link>
+						</li>
+						<li>
+							<Link to="/add-resource">Add Resources</Link>
+						</li>
+						<li>
+							<Link to="/add-inspiration">Add Inspiration</Link>
+						</li>
 					</ul>
-				</li>
-				<li>
-					<a href="#" data-toggle="collapse" aria-expanded="false">
-						<i className="fa fa-users"></i>
-						Blog Posts
-					</a>
-					<ul className="collapse list-unstyled" id="pageSubmenu2">
-						<li>Add Posts</li>
-						<li>Posts List</li>
-					</ul>
-				</li>
-				
-				
-				
-				
-				<li>
-					<a href="#">
-						<i className="fa fa-align-justify"></i>
-						Orders
-					</a>
-					<ul className="collapse list-unstyled" id="pageSubmenu3">
-						<li>Order List</li>
-					</ul>
-				</li>
-				<li>
-					<a href="#">
-						<i className="fa fa-calculator"></i>
-						User
-					</a>
-					<ul className="collapse list-unstyled" id="pageSubmenu8">
-						<li>Add User</li>
-						<li>Result List</li>
-					</ul>
-				</li>
-				<li>
-					<a href="#">
-						<i className="fa fa-cogs"></i>
-						Account Setting
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<i className="fa fa-align-left"></i>
-						FAQ
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<i className="fa fa-phone"></i>
-						Contact
-					</a>
-				</li>
-			</ul>
-		</nav>
-            </div>
-        );
-    }
+				</nav>
+			</div>
+		);
+	}
 }
 
 export default Sidemenu;
