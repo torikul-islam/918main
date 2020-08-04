f85ebd25af67d8a9e7796f6ae38a6e73e733afb9
Moksedul <mmislam1994@gmail.com>
2020-08-05 00:54:59 +0600

    responsive done

--- src/components/homeCreate.js
+++ src/components/homeCreate.js
@@ -11,7 +11,7 @@ const HomeCreate = ({ data, rooms, styles, onSelectOption, compname }) => {
             <div className="container">
                 <div className="list-furniture">
                     <ul>
-                        <li className="form-group col-sm-2">
+                        <li className="form-group col-md-2 col-5">
                             <select className="form-control" onChange={(e) => onSelectOption(e, 'rooms')}>
                                 <option value="">ROOM</option>
                                 {rooms.map((r, i) =>
@@ -19,7 +19,7 @@ const HomeCreate = ({ data, rooms, styles, onSelectOption, compname }) => {
                                 )}
                             </select>
                         </li>
-                        <li className="form-group col-sm-2">
+                        <li className="form-group col-md-2 col-5">
                             <select className="form-control" onChange={(e) => onSelectOption(e, 'styles')}>
                                 <option value="">STYLE</option>
                                 {styles.map((s, i) =>
