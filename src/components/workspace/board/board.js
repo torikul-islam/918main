5444256a41e0cdce9f2150625a0349a728b679c4
torikul <torikul>
2020-09-15 22:18:31 +0600

    image clicked log

--- src/components/workspace/board/board.js
+++ src/components/workspace/board/board.js
@@ -28,6 +28,7 @@ const Board = (props) => {
 
 
     function handleBoardItem(item) {
+        console.log('image clicked');
         setSelectedBoardItem(item);
 
     }
@@ -80,60 +81,60 @@ const Board = (props) => {
 
     return (
         <>
-        <div className="dragDrop">
-            <div className="container board-tilte">
-                <div className="boards-fiter">
+            <div className="dragDrop">
+                <div className="container board-tilte">
+                    <div className="boards-fiter">
 
-                    <ul className="for-desktop">
-                        <li><h4>Boards</h4></li>
-                        <select onChange={handleChangeBoardName}>
-                            {projectBoardName.map((item, i) =>
-                                <option key={i} value={item.uuid}>{item.name}</option>
-                            )}
-                        </select>
-                        {/* <li className="float-right">
+                        <ul className="for-desktop">
+                            <li><h4>Boards</h4></li>
+                            <select onChange={handleChangeBoardName}>
+                                {projectBoardName.map((item, i) =>
+                                    <option key={i} value={item.uuid}>{item.name}</option>
+                                )}
+                            </select>
+                            {/* <li className="float-right">
                             <img src={require('../../../Asset/Images/upicon.png')} alt="upicon.png" />
                         </li> */}
+                        </ul>
+                    </div>
+                    <div className="middle-body">
+                        {userProject.workspace_items && userProject.workspace_items.map((item, i) =>
+                            <Draggable key={i} bounds='parent'>
+                                <div onClick={() => handleBoardItem(item)} style={{ left: item.x_percent, top: item.y_percent, zIndex: item.z }}
+                                    className={`box boxoverlay ${selectedBoardItem.uuid === item.uuid ? "select-board-item" : ' '}`}>
+                                    <img style={{ width: item.width, height: item.height }}
+                                        src={(item.product && item.product.ref_img) || (item.inspiration && item.inspiration.ref_img)} alt="" />
+                                </div>
+                            </Draggable>
+                        )}
+                    </div>
+
+                </div>
+                <div className="bottom-icons">
+                    <ul>
+                        <li data-toggle="tooltip" data-placement="top" title="Copy workspace item"><img onClick={() => handleCopyItem(selectedBoardItem)} src={require('../../../Asset/Images/copypast.png')} alt="copypast.png" /></li>
+                        <li data-toggle="tooltip" data-placement="top" title="Delete item from workspace"><img onClick={() => handleDeleteItem(selectedBoardItem)} src={require('../../../Asset/Images/delete.png')} alt="delete.png" /></li>
+                        <li data-toggle="tooltip" data-placement="top" title="Move forward (in front of other item z-axis)" ><img onClick={() => handleZAxis('up', selectedBoardItem)} src={require('../../../Asset/Images/uparrwo.png')} alt="uparrwo.png" /></li>
+                        <li data-toggle="tooltip" data-placement="top" title="Move backward (behind other items z-axis)"><img onClick={() => handleZAxis('down', selectedBoardItem)} src={require('../../../Asset/Images/downarrow.png')} alt="downarrow.png" /></li>
                     </ul>
                 </div>
-                <div className="middle-body">
-                    {userProject.workspace_items && userProject.workspace_items.map((item, i) =>
-                        <Draggable key={i} bounds='parent'>
-                            <div onClick={() => handleBoardItem(item)} style={{ left: item.x_percent, top: item.y_percent, zIndex: item.z }}
-                                className={`box boxoverlay ${selectedBoardItem.uuid === item.uuid ? "select-board-item" : ' '}`}>
-                                <img style={{ width: item.width, height: item.height }}
-                                    src={(item.product && item.product.ref_img) || (item.inspiration && item.inspiration.ref_img)} alt="" />
-                            </div>
-                        </Draggable>
+                <div className="designerHelp">
+                    <button type="button">Desinger Help</button>
+                </div>
+
+            </div>
+            <ul className="for-mobile">
+                <li><h4>Boards</h4></li>
+                <select onChange={handleChangeBoardName}>
+                    {projectBoardName.map((item, i) =>
+                        <option key={i} value={item.uuid}>{item.name}</option>
                     )}
-                </div>
-
-            </div>
-            <div className="bottom-icons">
-                <ul>
-                    <li data-toggle="tooltip" data-placement="top" title="Copy workspace item"><img onClick={() => handleCopyItem(selectedBoardItem)} src={require('../../../Asset/Images/copypast.png')} alt="copypast.png" /></li>
-                    <li data-toggle="tooltip" data-placement="top" title="Delete item from workspace"><img onClick={() => handleDeleteItem(selectedBoardItem)} src={require('../../../Asset/Images/delete.png')} alt="delete.png" /></li>
-                    <li data-toggle="tooltip" data-placement="top" title="Move forward (in front of other item z-axis)" ><img onClick={() => handleZAxis('up', selectedBoardItem)} src={require('../../../Asset/Images/uparrwo.png')} alt="uparrwo.png" /></li>
-                    <li data-toggle="tooltip" data-placement="top" title="Move backward (behind other items z-axis)"><img onClick={() => handleZAxis('down', selectedBoardItem)} src={require('../../../Asset/Images/downarrow.png')} alt="downarrow.png" /></li>
-                </ul>
-            </div>
-            <div className="designerHelp">
-                <button type="button">Desinger Help</button>
-                </div>
-                
-        </div>
-        <ul className="for-mobile">
-                        <li><h4>Boards</h4></li>
-                        <select onChange={handleChangeBoardName}>
-                            {projectBoardName.map((item, i) =>
-                                <option key={i} value={item.uuid}>{item.name}</option>
-                            )}
-                        </select>
-                        {/* <li className="float-right">
+                </select>
+                {/* <li className="float-right">
                             <img src={require('../../../Asset/Images/upicon.png')} alt="upicon.png" />
                         </li> */}
-                    </ul>
-                    </>
+            </ul>
+        </>
     );
 };
 
